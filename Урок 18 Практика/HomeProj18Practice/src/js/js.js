eb4a46258636412ec7e83527b4f17ade7bcf903c
Ekaterina Kovylina <katuha.p97@gmail.com>
2017-10-14 00:03:16 +0700

    Practice lesson 18

---
+++ Урок 18 Практика/HomeProj18Practice/src/js/js.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+  $('.popup-btn').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').fadeIn();
+  });
+
+  $('.popup-close').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').fadeOut();
+  });
+
+  $('.features-slider').slick ({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+
+    responsive: [
+   
+       {
+         breakpoint: 992,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2,
+           prevArrow:'<button class="l-arrow arrow"></button>',
+           nextArrow:'<button class="r-arrow arrow"></button>',
+         }
+       },
+
+       {
+         breakpoint: 767,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1,
+           prevArrow:'<button class="l-arrow arrow"></button>',
+           nextArrow:'<button class="r-arrow arrow"></button>',
+         }
+       }
+       // You can unslick at a given breakpoint now by adding:
+       // settings: "unslick"
+       // instead of a settings object
+     ]
+
+  });
+});
