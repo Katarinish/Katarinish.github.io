d4742f28ff4375dde88c3c1e29aa1005c4ddbd23
Ekaterina Kovylina <katuha.p97@gmail.com>
2017-10-14 23:27:16 +0700

    19 practice

---
+++ Урок 19 Практика/HomeProj18Practice/src/js/js.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+  $('.popup-btn').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').fadeIn();
+  });
+
+  $('.popup-close').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').fadeOut();
+  });
+
+  $('.features-slider').slick ({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+
+    responsive: [
+   
+       {
+         breakpoint: 992,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2,
+           prevArrow:'<button class="l-arrow arrow"></button>',
+           nextArrow:'<button class="r-arrow arrow"></button>',
+         }
+       },
+
+       {
+         breakpoint: 767,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1,
+           prevArrow:'<button class="l-arrow arrow"></button>',
+           nextArrow:'<button class="r-arrow arrow"></button>',
+         }
+       }
+       // You can unslick at a given breakpoint now by adding:
+       // settings: "unslick"
+       // instead of a settings object
+     ]
+
+  });
+
+   $('.feedback-slider').slick({
+    infinite: true,
+
+    prevArrow:'<button class=" feed-arrow feed-l-arrow"><img src="../img/feedback/l-ar.svg" alt="" /></button>',
+    nextArrow:'<button class="feed-arrow feed-r-arrow"><img src="../img/feedback/r-ar.svg" alt="" /></button>',
+   });
+
+});
